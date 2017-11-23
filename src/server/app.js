2b6a226f21e9e79597046dd4d0bb6631bceaaea2
qw8880000 <qw8880000@126.com>
2017-11-23 11:15:08 +0800

    application struct |-- src/     |-- client     \-- server

---
+++ src/server/app.js
@@ -0,0 +1,33 @@
+
+'use strict';
+
+var express = require('express'), 
+  app = express(),
+  path = require('path'),
+  favicon = require('serve-favicon'),
+  logger = require('morgan'),
+  env = process.env.NODE_ENV || 'development';
+
+var clientPath = (env === 'development') ? './src/client':'./dist';
+
+app.set('port', process.env.PORT || 3002);
+app.set('bind-address', process.env.BIND_ADDRESS || 'localhost');
+
+// HTTP request logger
+app.use(logger('dev'));
+
+// serving public files
+app.use(express.static(clientPath));
+// serving a favicon
+// app.use(favicon(path.join(clientPath, '/favicon.ico')));
+
+// routes
+// Any deep link calls should return index.html
+app.use('/*', express.static(path.join(clientPath, '/index.html')));
+
+app.listen(app.get('port'), function() {
+    
+    console.log('Express server listening on port ' + app.get('port'));
+    console.log('\t http://' + app.get('bind-address') + ':' + app.get('port') + '\n');
+
+});
