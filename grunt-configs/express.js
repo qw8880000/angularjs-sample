7152bb779f848f48789e0c55bb133d76ff089f6e
qw8880000 <qw8880000@126.com>
2017-11-26 18:17:33 +0800

    [grunt] grunt-express-server

---
+++ grunt-configs/express.js
@@ -0,0 +1,24 @@
+
+module.exports = {
+
+    // 启动express server
+    express: {
+      options: {
+      },
+      dev: {
+        options: {
+          script: './src/server/app.js',
+          debug: true
+        }
+      },
+      dist: {
+        options: {
+          script: 'app.js',
+          node_env: 'production'
+        }
+      },
+      test: {
+        options: {}
+      }
+    }
+};
